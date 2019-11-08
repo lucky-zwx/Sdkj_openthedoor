42cf2d109e332c544a8e292a1f8d51b230dd1cf6
朱文骁 <34543176+zwx19981207@users.noreply.github.com>
2019-11-08 10:32:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/skip2/go-qrcode"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -33,6 +34,25 @@ func getting(context *gin.Context) {
 }
 
 func run()  {
+	login_cl := &http.Client{}
+	login_val := make(url.Values)
+	login_val.Set("iccode", "app_login")
+	login_val.Set("phone", "15065362098")
+	login_val.Set("pwd", "fa53d132a53d33931c51597fcfec0a74")
+	login_values := login_val.Encode()
+	req_login, err := http.NewRequest("POST", "http://epay.sdzy.cn:7110/school.ashx", strings.NewReader(login_values))
+
+	if err != nil {
+		log.Fatal("POST构建失败")
+	}
+	req_login.Header.Set("Cookie", "ASP.NET_SessionId=5ositwhlome30kbyxjmfrekq")
+	req_login.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req_login.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 7.1.2; vivo X9 Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36")
+
+	login_cl.Do(req_login)
+
+	defer req_login.Body.Close()
+
 	client := &http.Client{}
 	v := make(url.Values)
 	v.Set("iccode", "app_barcode")
@@ -41,9 +61,11 @@ func run()  {
 	v.Set("patroncode", "10000000")
 	valuse := v.Encode()
 	req, err := http.NewRequest("POST", "http://epay.sdzy.cn:7110/school.ashx", strings.NewReader(valuse))
+
 	if err != nil {
-		fmt.Println("")
+		log.Fatal("POST构建失败")
 	}
+
 	req.Header.Set("Cookie", "ASP.NET_SessionId=5ositwhlome30kbyxjmfrekq")
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 7.1.2; vivo X9 Build/N2G47H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36")
